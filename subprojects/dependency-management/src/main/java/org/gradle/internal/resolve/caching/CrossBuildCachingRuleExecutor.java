1623e788ec32f6944456c7dbb83d04977f477253
Cedric Champeau <cedric@gradle.com>
2018-05-18 13:22:18 +0200

    Implement discovery of rule inputs for injected services
    This commit replaces the not-so-nice service "snapshotting" with a proper
    implementation of input discovery for services. The idea is that rules can
    have injected services. Those services are usually blackboxes, but they
    may actually do work which is "expirable". Typically, a rule which fetches
    an external resource will need to re-execute once the resource is outdated.
    
    The problem is that the service is called _at execution time_, so we only
    know about the actual rule inputs once it has effectively been executed.
    To implement this capture, we need services to declare that if called,
    they will participate in some inputs. In that case, we record a "query"
    to the service, as well as the fingerprint of the answer to the query. Then
    the result of the rule is cached. Once we fetch the result from the cache,
    we ask the service if the result for the cached query is up-to-date. If not,
    we discard the cache entry, otherwise, we consider it's up-to-date.
    
    This removes the need for "snapshotting services" as we consider that a
    service which doesn't declare itself as contributing inputs has no side
    effect (it will always return the same value, independently of time).

--- subprojects/dependency-management/src/main/java/org/gradle/internal/resolve/caching/CrossBuildCachingRuleExecutor.java
+++ subprojects/dependency-management/src/main/java/org/gradle/internal/resolve/caching/CrossBuildCachingRuleExecutor.java
@@ -15,10 +15,11 @@
  */
 package org.gradle.internal.resolve.caching;
 
+import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Multimap;
 import org.gradle.api.Action;
 import org.gradle.api.Transformer;
-import org.gradle.api.internal.DependencyInjectingInstantiator;
 import org.gradle.api.internal.artifacts.configurations.dynamicversion.CachePolicy;
 import org.gradle.api.internal.changedetection.state.InMemoryCacheDecoratorFactory;
 import org.gradle.api.internal.changedetection.state.SnapshotSerializer;
@@ -32,19 +33,26 @@ import org.gradle.cache.PersistentCache;
 import org.gradle.cache.PersistentIndexedCache;
 import org.gradle.cache.PersistentIndexedCacheParameters;
 import org.gradle.cache.internal.filelock.LockOptionsBuilder;
+import org.gradle.internal.Cast;
+import org.gradle.internal.hash.HashCode;
 import org.gradle.internal.reflect.ConfigurableRule;
 import org.gradle.internal.reflect.InstantiatingAction;
 import org.gradle.internal.reflect.Instantiator;
 import org.gradle.internal.serialize.AbstractSerializer;
+import org.gradle.internal.serialize.BaseSerializerFactory;
 import org.gradle.internal.serialize.Decoder;
 import org.gradle.internal.serialize.Encoder;
 import org.gradle.internal.serialize.Serializer;
 import org.gradle.util.BuildCommencedTimeProvider;
 
+import javax.annotation.Nullable;
 import java.io.Closeable;
+import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 public class CrossBuildCachingRuleExecutor<KEY, DETAILS, RESULT> implements CachingRuleExecutor<KEY, DETAILS, RESULT>, Closeable {
     private final static Logger LOGGER = Logging.getLogger(CrossBuildCachingRuleExecutor.class);
@@ -110,20 +118,20 @@ public class CrossBuildCachingRuleExecutor<KEY, DETAILS, RESULT> implements Cach
         toBeSnapshotted.add(ketToSnapshottable.transform(key));
         Class<? extends Action<DETAILS>> ruleClass = rule.getRuleClass();
         Object[] ruleParams = rule.getRuleParams();
-
         toBeSnapshotted.add(ruleClass);
         toBeSnapshotted.add(ruleParams);
-        Instantiator instantiator = action.getInstantiator();
-        if (instantiator instanceof DependencyInjectingInstantiator) {
-            toBeSnapshotted.addAll(((DependencyInjectingInstantiator) instantiator).identifyInjectedServices(ruleClass, ruleParams));
-        }
         final ValueSnapshot snapshot = snapshotter.snapshot(toBeSnapshotted);
+        ImplicitInputsCapturingInstantiator instantiator = getInputCapturingInstantiator(action);
+        DefaultImplicitInputRegistrar registrar = new DefaultImplicitInputRegistrar();
+        if (instantiator != null) {
+            action = action.withInstantiator(instantiator.capturing(registrar));
+        }
         CachedEntry<RESULT> entry = store.get(snapshot);
         if (entry != null) {
             if (LOGGER.isDebugEnabled()) {
                 LOGGER.debug("Found result for rule {} and key {} in cache", rule, key);
             }
-            if (validator.isValid(cachePolicy, entry)) {
+            if (areImplicitInputsUpToDate(instantiator, key, rule, entry) && validator.isValid(cachePolicy, entry)) {
                 return entry.getResult();
             } else if (LOGGER.isDebugEnabled()) {
                 LOGGER.debug("Invalidating result for rule {} and key {} in cache", rule, key);
@@ -132,9 +140,33 @@ public class CrossBuildCachingRuleExecutor<KEY, DETAILS, RESULT> implements Cach
 
         RESULT result = executeRule(key, action, detailsToResult, onCacheMiss);
         if (result != null) {
-            store.put(snapshot, new CachedEntry<RESULT>(timeProvider.getCurrentTime(), result));
+            store.put(snapshot, new CachedEntry<RESULT>(timeProvider.getCurrentTime(), registrar.implicits, result));
         }
         return result;
+    }
+
+    private ImplicitInputsCapturingInstantiator getInputCapturingInstantiator(InstantiatingAction<DETAILS> action) {
+        Instantiator instantiator = action.getInstantiator();
+        if (instantiator instanceof ImplicitInputsCapturingInstantiator) {
+            return (ImplicitInputsCapturingInstantiator) instantiator;
+        }
+        return null;
+    }
+
+    private boolean areImplicitInputsUpToDate(ImplicitInputsCapturingInstantiator serviceRegistry, KEY key, ConfigurableRule<DETAILS> rule, CachedEntry<RESULT> entry) {
+        for (Map.Entry<String, Collection<ImplicitInputRecord<?, ?>>> implicitEntry : entry.getImplicits().asMap().entrySet()) {
+            String serviceName = implicitEntry.getKey();
+            ImplicitInputsProvidingService<Object, Object, ?> provider = Cast.uncheckedCast(serviceRegistry.findInputCapturingServiceByName(serviceName));
+            for (ImplicitInputRecord<?, ?> list : implicitEntry.getValue()) {
+                if (!provider.isUpToDate(list.getInput(), list.getOutput())) {
+                    if (LOGGER.isDebugEnabled()) {
+                        LOGGER.debug("Invalidating result for rule {} and key {} in cache because implicit input provided by service {} changed", rule, key, provider.getClass());
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     private <D extends DETAILS> RESULT executeRule(KEY key, InstantiatingAction<DETAILS> action, Transformer<RESULT, D> detailsToResult, Transformer<D, KEY> onCacheMiss) {
@@ -150,11 +182,17 @@ public class CrossBuildCachingRuleExecutor<KEY, DETAILS, RESULT> implements Cach
 
     public static class CachedEntry<RESULT> {
         private final long timestamp;
+        private final Multimap<String, ImplicitInputRecord<?, ?>> implicits;
         private final RESULT result;
 
-        private CachedEntry(long timestamp, RESULT result) {
+        private CachedEntry(long timestamp, Multimap<String, ImplicitInputRecord<?, ?>> implicits, RESULT result) {
             this.timestamp = timestamp;
+            this.implicits = implicits;
             this.result = result;
+        }
+
+        public Multimap<String, ImplicitInputRecord<?, ?>> getImplicits() {
+            return implicits;
         }
 
         public long getTimestamp() {
@@ -171,6 +209,7 @@ public class CrossBuildCachingRuleExecutor<KEY, DETAILS, RESULT> implements Cach
      * result is still valid or not. We cannot take that decision before
      * knowing the actual type of KEY, so we need to provide this as a
      * pluggable strategy when creating the executor.
+     *
      * @param <RESULT> the type of entry stored in the cache.
      */
     public interface EntryValidator<RESULT> {
@@ -179,6 +218,7 @@ public class CrossBuildCachingRuleExecutor<KEY, DETAILS, RESULT> implements Cach
 
     private static class CacheEntrySerializer<RESULT> extends AbstractSerializer<CachedEntry<RESULT>> {
         private final Serializer<RESULT> resultSerializer;
+        private final AnySerializer anySerializer = new AnySerializer();
 
         public CacheEntrySerializer(Serializer<RESULT> resultSerializer) {
             this.resultSerializer = resultSerializer;
@@ -186,13 +226,132 @@ public class CrossBuildCachingRuleExecutor<KEY, DETAILS, RESULT> implements Cach
 
         @Override
         public CachedEntry<RESULT> read(Decoder decoder) throws Exception {
-            return new CachedEntry<RESULT>(decoder.readLong(), resultSerializer.read(decoder));
+            return new CachedEntry<RESULT>(decoder.readLong(), readImplicits(decoder), resultSerializer.read(decoder));
+        }
+
+        private Multimap<String, ImplicitInputRecord<?, ?>> readImplicits(Decoder decoder) throws Exception {
+            int cpt = decoder.readSmallInt();
+            Multimap<String, ImplicitInputRecord<?, ?>> result = HashMultimap.create();
+            for (int i = 0; i < cpt; i++) {
+                String impl = decoder.readString();
+                List<ImplicitInputRecord<?, ?>> implicitInputOutputs = readImplicitList(decoder);
+                result.putAll(impl, implicitInputOutputs);
+            }
+            return result;
+        }
+
+        List<ImplicitInputRecord<?, ?>> readImplicitList(Decoder decoder) throws Exception {
+            int cpt = decoder.readSmallInt();
+            List<ImplicitInputRecord<?, ?>> implicits = Lists.newArrayListWithCapacity(cpt);
+            for (int i = 0; i < cpt; i++) {
+                final Object in = readAny(decoder);
+                final Object out = readAny(decoder);
+                implicits.add(new ImplicitInputRecord<Object, Object>() {
+                    @Override
+                    public Object getInput() {
+                        return in;
+                    }
+
+                    @Nullable
+                    @Override
+                    public Object getOutput() {
+                        return out;
+                    }
+                });
+            }
+            return implicits;
+        }
+
+        @Nullable
+        private Object readAny(Decoder decoder) throws Exception {
+            return anySerializer.read(decoder);
         }
 
         @Override
         public void write(Encoder encoder, CachedEntry<RESULT> value) throws Exception {
             encoder.writeLong(value.timestamp);
+            writeImplicits(encoder, value.implicits);
             resultSerializer.write(encoder, value.result);
+        }
+
+        private void writeImplicits(Encoder encoder, Multimap<String, ImplicitInputRecord<?, ?>> implicits) throws Exception {
+            encoder.writeSmallInt(implicits.size());
+            for (Map.Entry<String, Collection<ImplicitInputRecord<?, ?>>> entry : implicits.asMap().entrySet()) {
+                encoder.writeString(entry.getKey());
+                writeImplicitList(encoder, entry.getValue());
+            }
+        }
+
+        private void writeImplicitList(Encoder encoder, Collection<ImplicitInputRecord<?, ?>> implicits) throws Exception {
+            encoder.writeSmallInt(implicits.size());
+            for (ImplicitInputRecord<?, ?> implicit : implicits) {
+                writeAny(encoder, implicit.getInput());
+                writeAny(encoder, implicit.getOutput());
+            }
+        }
+
+        private void writeAny(Encoder encoder, Object any) throws Exception {
+            anySerializer.write(encoder, any);
+        }
+    }
+
+    private static class DefaultImplicitInputRegistrar implements ImplicitInputRecorder {
+        final Multimap<String, ImplicitInputRecord<?, ?>> implicits = HashMultimap.create();
+
+        @Override
+        public <IN, OUT> void register(String serviceName, ImplicitInputRecord<IN, OUT> input) {
+            implicits.put(serviceName, input);
+        }
+    }
+
+    private static class AnySerializer implements Serializer<Object> {
+        private static final BaseSerializerFactory SERIALIZER_FACTORY = new BaseSerializerFactory();
+
+        private static final Class<?>[] USUAL_TYPES = new Class[] {
+            String.class,
+            Boolean.class,
+            Long.class,
+            File.class,
+            byte[].class,
+            HashCode.class,
+            Throwable.class
+        };
+
+        @Override
+        public Object read(Decoder decoder) throws Exception {
+            int index = decoder.readSmallInt();
+            if (index == -1) {
+                return null;
+            }
+            Class<?> clazz;
+            if (index == -2) {
+                String typeName = decoder.readString();
+                clazz = Class.forName(typeName);
+            } else {
+                clazz = USUAL_TYPES[index];
+            }
+
+            return SERIALIZER_FACTORY.getSerializerFor(clazz).read(decoder);
+        }
+
+        @Override
+        public void write(Encoder encoder, Object value) throws Exception {
+            if (value == null) {
+                encoder.writeSmallInt(-1);
+                return;
+            }
+            Class<?> anyType = value.getClass();
+            Serializer<Object> serializer = Cast.uncheckedCast(SERIALIZER_FACTORY.getSerializerFor(anyType));
+            for (int i = 0; i < USUAL_TYPES.length; i++) {
+                if (USUAL_TYPES[i].equals(anyType)) {
+                    encoder.writeSmallInt(i);
+                    serializer.write(encoder, value);
+                    return;
+                }
+            }
+            encoder.writeSmallInt(-2);
+            encoder.writeString(anyType.getName());
+            serializer.write(encoder, value);
         }
     }
 }
